672c3a75a854bc75f62a586c47cad05870e5a15e
Nikola Chakarov <nikola386@gmail.com>
2025-11-04 11:03:15 +0200

    Add conditional drop for customers_email_key index

--- prisma/migrations/20251102163139_add_store_onboarded_flag/migration.sql
+++ prisma/migrations/20251102163139_add_store_onboarded_flag/migration.sql
@@ -1,5 +1,4 @@
--- DropIndex
-DROP INDEX "public"."customers_email_key";
+DROP INDEX IF EXISTS "public"."customers_email_key";
 
 -- Note: The onboarded column addition to stores table will be handled
 -- in migration 20251102181442_add_store_model when the stores table is created.
